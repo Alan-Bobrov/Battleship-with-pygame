e1334b6ed97e089f89fd003bc4cdaf36b3e3e3e0
Alan-Bobrov <159445827+Alan-Bobrov@users.noreply.github.com>
2024-07-29 13:02:14 +0300

    Update tests.py

--- tests.py
+++ tests.py
@@ -1,20 +1,20 @@
 from random import choice
 
 class Field:
-    def __init__(self, symbol="free_place", live_ships = 0, given_field = None) -> None:
+    def __init__(self, status="free_place", live_ships = 0, given_field = None) -> None:
         if given_field == None:
             field = [None for _ in range(11)]
             field[0] = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
             alpha = ("A", "B", "C", "D", "E", "F", "G", "H", "I", "J")
             for i in range(1, 11):
-                field[i] = [alpha[i - 1], symbol, symbol, symbol, symbol, symbol, symbol, symbol, symbol, symbol, symbol]
+                field[i] = [alpha[i - 1], status, status, status, status, status, status, status, status, status, status]
             self.field = field
         else:
             self.field = given_field
 
         self.live_ships = live_ships
     
-    def copying(self, field_copy, symbols) -> None:
+    def copying(self, field_copy, copyable_status):
         pass
 
 class Ship:
