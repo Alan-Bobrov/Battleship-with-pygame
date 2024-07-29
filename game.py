cfc50db83126cac5befc276a24e3d076ee9009f0
AngelesT85 <brawl-ford123456@mail.ru>
2024-07-29 14:59:12 +0300

    Update game.py

--- game.py
+++ game.py
@@ -6,9 +6,24 @@ pg.init()
 screen = pg.display.set_mode((1024, 900))
 screen.fill((255, 255, 255))
 
+OneDeckShipImg = pg.image.load("images/OneDeckShip.png")
+ShipStartImg = pg.image.load("images/ShipStart.png")
+ShipContinueImg = pg.image.load("images/ShipContinue.png")
+ShipEndImg = pg.image.load("images/ShipEnd.png")
+
+
+
 screen.blit(FieldImg, (0, 0))
 screen.blit(SkipImg, (142, 476))
 screen.blit(HitImg, (110, 476))
+
+screen.blit(OneDeckShipImg, (110, 508))
+
+screen.blit(ShipStartImg, (110, 604))
+screen.blit(ShipContinueImg, (110, 570))
+screen.blit(ShipEndImg, (110, 540))
+
+screen.blit(HitImg, (110, 508))
 
 is_game = True
 while is_game:
