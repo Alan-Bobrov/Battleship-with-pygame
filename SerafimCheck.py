b54f0e077082d3bed602e5eaab25d9f119fb1981
AngelesT85 <brawl-ford123456@mail.ru>
2024-07-29 21:36:09 +0300

    Update SerafimCheck.py

--- SerafimCheck.py
+++ SerafimCheck.py
@@ -6,8 +6,25 @@ def print_field(field):
     for i in range(len(field)):
         print(*field[i])
 
+def put_ship(field, coords):
+    num_of_ships_around = 0
+    string, column = coords
+    for i in (1, -1):
+        if field[string][column + i] == "*":
+            num_of_ships_around += 1
+    for i in (1, -1):
+        if field[string + i][column] == "*":
+            num_of_ships_around += 1 
 
+    if num_of_ships_around <= 1:
+        field[string][column] = "*"
+    return num_of_ships_around
 
 field = create_field()
-field[4][1] = "*"
+a = put_ship(field, (4, 5))
+c = put_ship(field, (4, 7))
+b = put_ship(field, (4, 6))
 print_field(field)
+print(a)
+print(c)
+print(b)
