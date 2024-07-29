1dd61159dc4288a74b09fdf719e602d5906a984d
Alan-Bobrov <159445827+Alan-Bobrov@users.noreply.github.com>
2024-07-29 13:48:27 +0300

    Update classes.py

--- classes.py
+++ classes.py
@@ -1,8 +1,12 @@
 import pygame as pg
 
 class Field:
-    def __init__(self) -> None:
+    def __init__(self, live_ships=9) -> None:
         self.image = "images/Field.png"
+        self.live_ships = live_ships
+
+    def copying(self, field_copy, copyable_status):
+        pass
 
 field = Field()
 
