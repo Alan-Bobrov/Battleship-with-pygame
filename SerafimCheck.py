17e1da9de4204d8ca022e4a35e592ef8bbfd0cee
AngelesT85 <brawl-ford123456@mail.ru>
2024-07-29 20:43:54 +0300

    Update SerafimCheck.py

--- SerafimCheck.py
+++ SerafimCheck.py
@@ -1,10 +1,13 @@
+def create_field(space_symbol="-"):
+    field = [[space_symbol for _ in range(10)] for i in range(10)]
+    return field
+
+def print_field(field):
+    for i in range(len(field)):
+        print(*field[i])
 
 
-y_coords = []
 
-for i in range(10):
-    first_num = 476 + 32 * i
-    second_num = first_num + 28
-    y_coords.append((first_num, second_num))
-
-print(y_coords)
+field = create_field()
+field[4][1] = "*"
+print_field(field)
