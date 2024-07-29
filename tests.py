5138feff1d4b2c1fdfb9fde324553ec5d21c8810
AngelesT85 <brawl-ford123456@mail.ru>
2024-07-29 17:59:15 +0300

    Update tests.py

--- tests.py
+++ tests.py
@@ -1,14 +1,10 @@
 
 
+y_coords = []
 
+for i in range(10):
+    first_num = 476 + 32 * i
+    second_num = first_num + 28
+    y_coords.append((first_num, second_num))
 
-
-
-
-
-
-
-
-
-
-
+print(y_coords)
