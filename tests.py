ec136eb1ab79afd418fd9c6e0710cd916a0d333b
Alan-Bobrov <159445827+Alan-Bobrov@users.noreply.github.com>
2024-07-29 13:53:09 +0300

    Update tests.py

--- tests.py
+++ tests.py
@@ -1,14 +1,3 @@
-from random import choice
-
-class Field:
-    def __init__(self, status="free_place", live_ships = 9, given_field = None) -> None:
-        if given_field == None:
-            field = []
-            for _ in range(1, 11):
-                field.append([status, status, status, status, status, status, status, status, status, status])
-            self.field = field
-        else:
-            self.field = given_field
 
 
 
