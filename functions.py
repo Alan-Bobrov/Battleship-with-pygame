c5c475156861f20cff707334ca1e52fffdc3bd20
Alan-Bobrov <159445827+Alan-Bobrov@users.noreply.github.com>
2024-07-30 13:14:06 +0300

    Update functions.py

--- functions.py
+++ functions.py
@@ -1,11 +1,10 @@
 def new_ship():
     pass
+
 def test_all_cells():
     pass
-def player_play():
-    pass
 
-def bot_play():
+def player_play():
     pass
 
 def test_death():
@@ -13,9 +12,3 @@ def test_death():
   
 def play():
     pass
-
-def fire_pg(screen, x, y, x_coords, y_coords, Img):
-    for iy in range(10):
-        for jx in range(10):
-            if (x_coords[jx][0] <= x <= x_coords[jx][1]) and (y_coords[iy][0] <= y <= y_coords[iy][1]):
-                screen.blit(Img, (x_coords[jx][0], y_coords[iy][0]))
