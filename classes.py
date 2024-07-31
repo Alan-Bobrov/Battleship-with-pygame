d46b9c1d9ca6b265148760f35519377c64a4e4b8
Alan-Bobrov <159445827+Alan-Bobrov@users.noreply.github.com>
2024-07-31 16:05:52 +0300

    Update classes.py

--- classes.py
+++ classes.py
@@ -3,7 +3,6 @@ from random import randint
 
 class Field:
     def __init__(self, first_X, first_Y, live_ships=9) -> None: #110 476 - first field and 598 476 - second
-        self.image = "images/Field.png"
         self.live_ships = live_ships
         self.field = [[Place(first_X + j * 32, first_Y + i * 32) for j in range(10)] for i in range(10)]
 
@@ -25,7 +24,10 @@ class Field:
         while True:
             X = randint(0, 9) * 32 + 110
             Y = randint(0, 9) * 32 + 476
-            if self.fire_pg(X, Y):
+            end = self.fire_pg(X, Y)
+            if end[0]:
+                if end[1]:
+                    self.death(end[2][0], end[2][1])
                 break
     
     def bot_do_ships(self): #эта функция расстановления ботом кораблей, она врененная
@@ -37,7 +39,7 @@ class Field:
                 self.field[Y][X].images.append(pg.image.load("images/OneDeckShip.png"))
                 break
     
-    def fire_pg(self, x, y) -> bool:
+    def fire_pg(self, x, y) -> tuple:
         for iy in range(10):
             for jx in range(10):
                 X = self.field[iy][jx].X
@@ -46,13 +48,33 @@ class Field:
                     if self.field[iy][jx].status == "free_place":
                         self.field[iy][jx].status = "skip"
                         self.field[iy][jx].images.append(pg.image.load("images/Skip.png"))
-                        return True
+                        return True, False
                     elif self.field[iy][jx].status == "part_ship":
                         self.field[iy][jx].status = "hit"
                         self.field[iy][jx].images.append(pg.image.load("images/Hit.png"))
-                        return True
-        return False
+                        self.death(jx, iy)
+                        return True, True, (iy, jx)
+        return False, False
 
+    def death(self, x, y):
+        list_coords = [(x - 1, y - 1), (x, y - 1), (x + 1, y - 1), (x - 1, y), (x + 1, y), (x - 1, y + 1), (x, y + 1), (x + 1, y + 1)]
+        list_coords_2 = []
+        list_live_ships = []
+        list_hit_ships = []
+        for i in list_coords:
+            if 0 <= i[0] <= 9 and 0 <= i[1] <= 9:
+                list_coords_2.append(i)
+                if self.field[i[1]][i[0]].status == "part_ship":
+                    list_live_ships.append(self.field[i[0]][i[1]])
+                elif self.field[i[1]][i[0]].status == "hit":
+                    list_hit_ships.append(self.field[i[0]][i[1]])
+
+        if len(list_hit_ships) == 0 and len(list_live_ships) == 0:
+            for i in list_coords_2:
+                self.field[i[1]][i[0]].status = "skip"
+                self.field[i[1]][i[0]].images.append(pg.image.load("images/Skip.png"))
+                self.live_ships -= 1
+        
 
 class Place:
     def __init__(self, X, Y) -> None:
@@ -69,11 +91,11 @@ class Hit:
     def __init__(self) -> None:
         self.image = "images/Hit.png"
 
-field = Field(110, 476)
-skip = Skip()
-hit = Hit()
+#field = Field(110, 476)
+#skip = Skip()
+#hit = Hit()
 
 
-FieldImg = pg.image.load(field.image)
-SkipImg = pg.image.load(skip.image)
-HitImg = pg.image.load(hit.image)
+#FieldImg = pg.image.load(field.image)
+#SkipImg = pg.image.load(skip.image)
+#HitImg = pg.image.load(hit.image)
