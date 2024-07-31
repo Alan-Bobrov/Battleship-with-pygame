3a93a245348b0b1b8155e749ff7550c40435874a
Alan-Bobrov <159445827+Alan-Bobrov@users.noreply.github.com>
2024-07-31 13:13:43 +0300

    Update game.py

--- game.py
+++ game.py
@@ -1,6 +1,7 @@
 import pygame as pg
 from classes import *
 from functions import *
+from random import randint
 
 pg.init()
 
@@ -14,7 +15,7 @@ bot_field = Field(598, 476)
 
 # цикл под этим коментом отвечает за стартовые корабли бота, можешь его убрать, он так работает временно
 for _ in range(10):
-    bot_field.bot_play()
+    bot_field.bot_do_ships()
 
 # пока что это не нужно, но я это не удалил, мб в будующем пригодится
 #for i in range(10):
@@ -26,22 +27,20 @@ for _ in range(10):
 screen = pg.display.set_mode((1024, 900))
 screen.fill((255, 255, 255))
 
-OneDeckShipImg = pg.image.load("images/OneDeckShip.png")
-ShipStartImg = pg.image.load("images/ShipStart.png")
-ShipContinueImg = pg.image.load("images/ShipContinue.png")
-ShipEndImg = pg.image.load("images/ShipEnd.png")
+#OneDeckShipImg = pg.image.load("images/OneDeckShip.png")
+#ShipStartImg = pg.image.load("images/ShipStart.png")
+#ShipContinueImg = pg.image.load("images/ShipContinue.png")
+#ShipEndImg = pg.image.load("images/ShipEnd.png")
 
-
-# чтобы по этим кораблям стрелять нужно их расставить по другому, а мне это делать лень
 screen.blit(FieldImg, (0, 0))
 # screen.blit(SkipImg, (142, 476))
 # screen.blit(HitImg, (110, 476))
 
-screen.blit(OneDeckShipImg, (110, 508))
+#screen.blit(OneDeckShipImg, (110, 508))
 
-screen.blit(ShipStartImg, (110, 604))
-screen.blit(ShipContinueImg, (110, 570))
-screen.blit(ShipEndImg, (110, 540))
+#screen.blit(ShipStartImg, (110, 604))
+#screen.blit(ShipContinueImg, (110, 570))
+#screen.blit(ShipEndImg, (110, 540))
 
 is_game = True
 while is_game:
@@ -53,9 +52,7 @@ while is_game:
         
         elif event.type == pg.MOUSEBUTTONDOWN:
             x, y = pg.mouse.get_pos() # 467, 560
-            bot_field.fire_pg(x, y) 
-            #это чтобы стрелять по боту, можешь изменить bot_field на player_field, чтобы стрелять по игроку
+            if bot_field.fire_pg(x, y):
+                player_field.bot_play()
 
     pg.display.flip()
-
-# если хочешь узнать где я работал, заиди в классы и посмотри что я сделал с классами
