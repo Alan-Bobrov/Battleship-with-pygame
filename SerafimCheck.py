ef5498d018e01970e4cf8b61a036af83399418c4
AngelesT85 <brawl-ford123456@mail.ru>
2024-08-04 15:59:31 +0300

    Update SerafimCheck.py

--- SerafimCheck.py
+++ SerafimCheck.py
@@ -109,7 +109,6 @@ class Ship:
                         return False, None
                     return True, ("Right" if i == 1 else "Left")
 
-
                 # if vertical ship
                 if isinstance(string_cell, Ship):
                     column_values = list()
@@ -132,53 +131,52 @@ class Ship:
 
         return False, None
 
+    # func for put ship on comp field
+    def create_ship(comp_field, user_field, coords):
+        ship = Ship()
+        result = ship.put_ship(comp_field, user_field, coords)
+        return result
+    
+    # func for random create_ship()
+    def random_ship_gen(comp_field, user_field, num_of_ships):
+        while num_of_ships < 10:
+            result = Ship.create_ship(comp_field, user_field, (randint(0, 9), randint(0, 9)))
+            if result[1] == "new":
+                num_of_ships += 1
+        return num_of_ships
+
 return_num_ships()
 
+Ship.create_ship(comp_field, user_field, (0, 0))
 
-def create_ship(comp_field, user_field, coords):
-    ship = Ship()
-    result = ship.put_ship(comp_field, user_field, coords)
-    return result
+Ship.create_ship(comp_field, user_field, (5, 4))
+Ship.create_ship(comp_field, user_field, (5, 5))
+Ship.create_ship(comp_field, user_field, (5, 6))
 
-def random_ship_gen(comp_field, user_field, num_of_ships):
-    while num_of_ships < 10:
-        result = create_ship(comp_field, user_field, (randint(0, 9), randint(0, 9)))
-        if result[1] == "new":
-            num_of_ships += 1
-    return num_of_ships
+Ship.create_ship(comp_field, user_field, (9, 9))
 
-# random_ship_gen(comp_field, user_field, num_of_ships)
+Ship.create_ship(comp_field, user_field, (0, 6))
+Ship.create_ship(comp_field, user_field, (0, 7))
 
-create_ship(comp_field, user_field, (0, 0))
+Ship.create_ship(comp_field, user_field, (2, 9))
+Ship.create_ship(comp_field, user_field, (3, 9))
+Ship.create_ship(comp_field, user_field, (4, 9))
+Ship.create_ship(comp_field, user_field, (5, 9))
 
-create_ship(comp_field, user_field, (5, 4))
-create_ship(comp_field, user_field, (5, 5))
-create_ship(comp_field, user_field, (5, 6))
+Ship.create_ship(comp_field, user_field, (7, 9))
 
-create_ship(comp_field, user_field, (9, 9))
+Ship.create_ship(comp_field, user_field, (9, 0))
+Ship.create_ship(comp_field, user_field, (9, 1))
 
-create_ship(comp_field, user_field, (0, 6))
-create_ship(comp_field, user_field, (0, 7))
+Ship.create_ship(comp_field, user_field, (9, 2))
 
-create_ship(comp_field, user_field, (2, 9))
-create_ship(comp_field, user_field, (3, 9))
-create_ship(comp_field, user_field, (4, 9))
-create_ship(comp_field, user_field, (5, 9))
+Ship.create_ship(comp_field, user_field, (2, 0))
+Ship.create_ship(comp_field, user_field, (2, 1))
 
-create_ship(comp_field, user_field, (7, 9))
+Ship.create_ship(comp_field, user_field, (2, 4))
+Ship.create_ship(comp_field, user_field, (2, 5))
 
-create_ship(comp_field, user_field, (9, 0))
-create_ship(comp_field, user_field, (9, 1))
-
-create_ship(comp_field, user_field, (9, 2))
-
-create_ship(comp_field, user_field, (2, 0))
-create_ship(comp_field, user_field, (2, 1))
-
-create_ship(comp_field, user_field, (2, 4))
-create_ship(comp_field, user_field, (2, 5))
-
-create_ship(comp_field, user_field, (7, 0))
+Ship.create_ship(comp_field, user_field, (7, 0))
 
 print_field(user_field)
 # print("---------------------------------------")
