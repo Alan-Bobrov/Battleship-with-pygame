20c2d5fbbb66c1827c5c1d55f586587987795e9b
Alan-Bobrov <159445827+Alan-Bobrov@users.noreply.github.com>
2024-08-05 11:30:28 +0300

    Update game.py

--- game.py
+++ game.py
@@ -1,39 +1,21 @@
 import pygame as pg
 from classes import *
 from functions import *
+from images import *
 
 pg.init()
-
-#x_coords = []
-#y_coords = []
-# our field: - 366
-# enemy field: + 122
 
 player_field = Field(110, 476)
 bot_field = Field(598, 476)
 
-# цикл под этим коментом отвечает за стартовые корабли и бота и игрока, можешь его убрать, он так работает временно
-for _ in range(10):
-    bot_field.bot_do_ships()
-for _ in range(10):
-    player_field.bot_do_ships()
 
-# пока что это не нужно, но я это не удалил, мб в будующем пригодится
-#for i in range(10):
-#    first_num = 476 + 32 * i
-#    second_num = first_num + 28
-#    y_coords.append((first_num, second_num))
-#    x_coords.append((first_num - 366, second_num - 366))
+bot_field.bot_do_ships()
+bot_field.normal_ships_image()
 
 screen = pg.display.set_mode((1024, 900))
 screen.fill((255, 255, 255))
 
-#OneDeckShipImg = pg.image.load("images/OneDeckShip.png")
-#ShipStartImg = pg.image.load("images/ShipStart.png")
-#ShipContinueImg = pg.image.load("images/ShipContinue.png")
-#ShipEndImg = pg.image.load("images/ShipEnd.png")
-
-screen.blit(pg.image.load("images/Field.png"), (0, 0))
+screen.blit(FieldImg, (0, 0))
 
 is_game = True
 while is_game:
@@ -44,7 +26,7 @@ while is_game:
             is_game = False
         
         elif event.type == pg.MOUSEBUTTONDOWN:
-            x, y = pg.mouse.get_pos() # 467, 560
+            x, y = pg.mouse.get_pos()
             end_bots_attack = bot_field.fire_pg(x, y)
             if end_bots_attack[0]:
                 player_field.bot_play()
