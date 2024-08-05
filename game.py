ecef09c1b5dded71d0162baa4e01af42eeb1f106
Alan-Bobrov <159445827+Alan-Bobrov@users.noreply.github.com>
2024-08-05 11:43:06 +0300

    Update game.py

--- game.py
+++ game.py
@@ -16,12 +16,8 @@ screen = pg.display.set_mode((1024, 900))
 screen.fill((255, 255, 255))
 
 screen.blit(FieldImg, (0, 0))
-<<<<<<< HEAD
-
-=======
 is_again = False
 is_putting = True
->>>>>>> bf4315c31f8c78d1c28b8d8073eb66dc35d7732c
 is_game = True
 while is_game:
     player_field.pr_all(screen, print_ships=True)
@@ -32,13 +28,9 @@ while is_game:
             is_game = False
         
         elif event.type == pg.MOUSEBUTTONDOWN:
-<<<<<<< HEAD
-            x, y = pg.mouse.get_pos()
-=======
             x, y = pg.mouse.get_pos() # 467, 560
             if (81 <= x <= 941) and (55 <= y <= 189) and is_putting:
                 clear_field(screen)
->>>>>>> bf4315c31f8c78d1c28b8d8073eb66dc35d7732c
             end_bots_attack = bot_field.fire_pg(x, y)
             if end_bots_attack[0]:
                 player_field.bot_play()
