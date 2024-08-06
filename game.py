ade6161ec54e1f6a8a2ddd2a3489aebea732004a
AngelesT85 <brawl-ford123456@mail.ru>
2024-08-06 10:19:27 +0300

    Update game.py

--- game.py
+++ game.py
@@ -1,29 +1,16 @@
 import pygame as pg
 from classes import *
 from functions import *
+from images import *
 
 pg.init()
-
-#x_coords = []
-#y_coords = []
-# our field: - 366
-# enemy field: + 122
 
 player_field = Field(110, 476)
 bot_field = Field(598, 476)
 
-# цикл под этим коментом отвечает за стартовые корабли и бота и игрока, можешь его убрать, он так работает временно
-for _ in range(10):
-    bot_field.bot_do_ships()
-for _ in range(10):
-    player_field.bot_do_ships()
 
-# пока что это не нужно, но я это не удалил, мб в будующем пригодится
-#for i in range(10):
-#    first_num = 476 + 32 * i
-#    second_num = first_num + 28
-#    y_coords.append((first_num, second_num))
-#    x_coords.append((first_num - 366, second_num - 366))
+bot_field.bot_do_ships()
+bot_field.normal_ships_image()
 
 screen = pg.display.set_mode((1024, 900))
 screen.fill((255, 255, 255))
