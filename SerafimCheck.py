70ff5982968391ad254fe604b03972e49e7fe9f2
AngelesT85 <brawl-ford123456@mail.ru>
2024-08-07 10:23:06 +0300

    Update SerafimCheck.py

--- SerafimCheck.py
+++ SerafimCheck.py
@@ -10,37 +10,42 @@ def create_ship(comp_field, coords):
     ship = Ship()
     result = ship.put_ship(comp_field, coords)
     return result
-    
-# func for random create_ship()
-def random_ship_gen(comp_field, num_of_ships):
-    while num_of_ships < 10:
-        result = create_ship(comp_field, (randint(0, 9), randint(0, 9)))
-        if result[1] == "new":
-            num_of_ships += 1
-    return num_of_ships
 
 return_num_ships()
 
-num_of_ships = 0
+# func for random create_ship()
+def random_ship_gen(comp_field, num_of_ships):
+    num_of_errors = 0
+    while num_of_ships < 10:
 
-# random_ship_gen(comp_field, num_of_ships)
+        # if num_of_errors >= 200:
+        #     num_of_errors = 0
+        #     comp_field = create_field()
+        #     num_of_ships = 0
+        #     return_num_ships()
 
-col = 5
-var = 4
-ship_len = 4
+        result = create_ship(comp_field, (randint(0, 9), randint(0, 9)))
+        if not result[0]:
+            num_of_errors += 1
+
+        elif result[1] == "new":
+            num_of_ships += 1
+
+    return num_of_ships
 
 
-for i in range(ship_len):
-    create_ship(comp_field, (col, i + 2))
 
-for i in range(ship_len):
-    Ship.fire(Ship, comp_field, (col, i + 2))
-
-create_ship(comp_field, (9, 9))
-
-Ship.fire(Ship, comp_field, (9, 9))
-
+num_of_ships = 10
+random_ship_gen(comp_field, 0)
 print_field(comp_field)
+
+while num_of_ships >= 1:
+    coords = list(map(int, input().split()))
+    result = Ship.fire(Ship, comp_field, coords)
+    if result[1] == "Death":
+        num_of_ships -= 1
+    print_field(comp_field)
+
 # print(comp_field[5][2].start_coords)
 
 
