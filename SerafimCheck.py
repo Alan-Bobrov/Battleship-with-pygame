4d9bff70bfbf13eae4a1a2a99c2f77c4b8c2222b
AngelesT85 <brawl-ford123456@mail.ru>
2024-08-07 09:37:33 +0300

    Update SerafimCheck.py

--- SerafimCheck.py
+++ SerafimCheck.py
@@ -25,25 +25,22 @@ num_of_ships = 0
 
 # random_ship_gen(comp_field, num_of_ships)
 
-col = 9
-var = 1
+col = 5
+var = 4
 ship_len = 4
 
-for i in range(ship_len):
-    create_ship(comp_field, (i + var, col))
 
 for i in range(ship_len):
-    Ship.fire(Ship, comp_field, (i + var, col))
+    create_ship(comp_field, (col, i + 2))
+
+for i in range(ship_len):
+    Ship.fire(Ship, comp_field, (col, i + 2))
 
 create_ship(comp_field, (9, 9))
 
 Ship.fire(Ship, comp_field, (9, 9))
 
 print_field(comp_field)
-
-
-
-comp_field = create_field()
-user_field = create_field()
+# print(comp_field[5][2].start_coords)
 
 
