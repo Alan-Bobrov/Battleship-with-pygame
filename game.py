6b8bd7b81dfa189ebddcce26812b894ee0c28a7b
Alan-Bobrov <159445827+Alan-Bobrov@users.noreply.github.com>
2024-08-08 09:15:54 +0300

    Update game.py

--- game.py
+++ game.py
@@ -32,8 +32,9 @@ def game():
     num_of_ships = 0 # number of ships the player has placed
     do_ship = True # is the player currently placing ships
     player_comp_field = create_field()
-    player_ship_count = 0 
+    player_ship_count = 0
     bot_ship_count = 0
+    first_move = False
 
     while is_game:
         screen.blit(FieldImg, (0, 0))
@@ -52,12 +53,11 @@ def game():
                 if j.status == "part_ship":
                     bot_ship_count += 1
 
-
-        if player_ship_count == 0:
+        if  player_ship_count == 0 and first_move:
             # the message "You lose!" appears here
             # screen.blit(LoseImg or another name, (0, 0))
             pass
-        elif bot_comp_field == 0:
+        if  bot_ship_count == 0  and first_move:
             # the message "You win!" appears here
             # screen.blit(WinImg or another name, (0, 0))
             pass
@@ -95,8 +95,7 @@ def game():
                         # the player completes the placement of ships
                         do_ship = False
 
-                else:
-                    
+                else:           
                     changed, x, y = change_coords(x, y, 598, 476)
                     if changed and bot_field.field[y][x].status in ("free_place", "part_ship"):
                         s = Ship()
@@ -104,6 +103,7 @@ def game():
                         players_attack_result = s.fire(bot_comp_field, (y, x))
                         bot_field.synchronize(x, y)
                         bot_field.synchronize(x, y, bot_comp_field)
+                        first_move = True
                         # if the player misses, then the bot's turn begins
                         if players_attack_result[0] == False:
                             bot_move = (True,)
