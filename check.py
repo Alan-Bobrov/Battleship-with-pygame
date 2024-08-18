b4d43c2c9acd58acf1c308981ab84da8d654bc13
AngelesT85 <brawl-ford123456@mail.ru>
2024-08-18 17:02:38 +0300

    Update check.py

--- check.py
+++ check.py
@@ -1,3 +1,26 @@
-a = [((6, 5), "hit"), ((1, 2), "hit")]
-a.sort()
-print(a)
+from classes import *
+from functions import *
+from time import sleep
+
+return_num_ships()
+
+field = create_field()
+
+Ship.create_ship(field, (5, 5))
+Ship.create_ship(field, (6, 5))
+Ship.create_ship(field, (7, 5))
+print_field(field)
+print("--------------------")
+sleep(5)
+
+bot = Bot()
+
+
+for i in range(20):
+    bot.fire(field)
+    print_field(field)
+    print("--------------------------------")
+    sleep(1)
+
+
+
