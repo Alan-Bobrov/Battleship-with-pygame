405cc32e503de166eb981041d946c5496020d6c2
AngelesT85 <brawl-ford123456@mail.ru>
2024-08-21 22:11:28 +0300

    Update check.py

--- check.py
+++ check.py
@@ -4,21 +4,17 @@ from time import sleep
 
 field = create_field()
 
-Ship.ship_gen(field, 0, True, 0)
-print_field(field)
-print("--------------------")
-# sleep(5)
-
-bot = Bot()
-
-
-for i in range(50):
-    bot.fire(field)
+while True:
+    Ship.ship_gen(field, 0, True, 0)
+    for i in range(len(field)):
+        for j in range(len(field)):
+            if isinstance(field[i][j], Ship):
+                field[i][j] = "o"
     print_field(field)
-    print("--------------------------------")
-#     # sleep(1)
-
-return_num_ships()
+    print("--------------------")
+    field= create_field()
+    input()
+    return_num_ships()
 
 
 
