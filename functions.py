e334decfc9802ae0213695c5494573ee0c1b3389
AngelesT85 <brawl-ford123456@mail.ru>
2024-08-22 15:39:17 +0300

    Update functions.py

--- functions.py
+++ functions.py
@@ -23,16 +23,17 @@ def print_field(field):
 def update_num_of_ships(cell):
     with open("num_of_ships.json", "r", encoding="utf-8") as num_of_ships:
         num_of_ships = json.load(num_of_ships)
-        try:
-            if num_of_ships[f"{cell.length}"] < 1:
-                return False
-            num_of_ships[f"{cell.length - 1}"] += 1
-            num_of_ships[f"{cell.length}"] -= 1
-        except:
-            pass
-        else:
-            with open("num_of_ships.json", "w", encoding="utf-8") as file:
-                json.dump(num_of_ships, file, indent=4)
+
+        # if ships with such length is over
+        if num_of_ships[f"{cell.length}"] < 1:
+            return False
+            
+        num_of_ships[f"{cell.length - 1}"] += 1
+        num_of_ships[f"{cell.length}"] -= 1
+
+        with open("num_of_ships.json", "w", encoding="utf-8") as file:
+            json.dump(num_of_ships, file, indent=4)
+            
     return True
 
 def change_coords(x, y, first_x, first_y):
