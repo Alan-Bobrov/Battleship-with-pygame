383523c2df193c932f3f132d786c3d7d4647827f
Alan-Bobrov <159445827+Alan-Bobrov@users.noreply.github.com>
2024-08-22 17:08:39 +0300

    Update game.py

--- game.py
+++ game.py
@@ -28,12 +28,17 @@ def game():
     screen = pg.display.set_mode((1024, 900))
     screen.fill((255, 255, 255))
 
+    # import values of settings
     with open("settings.json", "r", encoding="utf-8") as settings:
         settings = json.load(settings)
+
         ShowEnemyShips = settings["Show Enemy Ships"]
         ShowYourShips = settings["Show Your Ships"]
         InfinityYourMoves = settings["Infinity Your Moves"]
         InfinityEnemyMoves = settings["Infinity Enemy Moves"]
+        RandomShipGen = settings["Random Ship Generation"]
+        PrintUserCompField = settings["Print User Comp Field"]
+        PrintCompCompField = settings["Print Comp Comp Field"]
 
     is_game = True
     num_of_ships = 0 # number of ships the player has placed
@@ -79,6 +84,9 @@ def game():
         if can_go and do_ship:
             screen.blit(RandomShipGenImg, (0, 0))
 
+        if can_go and do_ship:
+            screen.blit(RandomShipGenImg, (0, 0))
+
         player_ship_count = 0    
         bot_ship_count = 0
 
@@ -106,7 +114,11 @@ def game():
 
                 # player arranges ships
                 if do_ship:
+<<<<<<< HEAD
                     if (92 <= x <= 932) and (222 <= y <= 369):
+=======
+                    if RandomShipGen or ((92 <= x <= 932) and (222 <= y <= 369)):
+>>>>>>> 23060fd99ed3cd116de677efb15795b0b8b78ba2
                         return_num_ships()
                         player_field = Field(108, 474)
                         player_comp_field = create_field()
@@ -128,6 +140,7 @@ def game():
                 elif can_go:
                     changed, x, y = change_coords(x, y, 598, 476)
                     if changed and bot_field.field[y][x].status in ("free_place", "part_ship"):
+
                         s = Ship()
 
                         # the player makes a move
@@ -136,6 +149,12 @@ def game():
                         bot_field.synchronize(x, y, bot_comp_field)
                         first_move = True
                         bot_field.pr_all(screen, print_ships=ShowEnemyShips)
+
+                        # print user field for comp
+                        if PrintCompCompField:
+                            print("Bot Field")
+                            print_field(bot_comp_field)
+                            print("------------------------------------")
 
                         if InfinityYourMoves:
                             players_attack_result = (True,)
@@ -146,9 +165,19 @@ def game():
                             
                             while bot_move:
                                 sleep(0.125)
+<<<<<<< HEAD
+=======
+                                
+>>>>>>> 23060fd99ed3cd116de677efb15795b0b8b78ba2
                                 # the bot chooses the place where it goes
                                 bot_move, coords = bot_ob.cell_selection(player_comp_field) # y x
                                 y, x = coords
+
+                                # print bot field for comp
+                                if PrintUserCompField:
+                                    print("User Field")
+                                    print_field(player_comp_field)
+                                    print("-----------------------------------------")
 
                                 # the bot makes a move on the place that he has chosen in advance
                                 player_field.synchronize(x, y)
