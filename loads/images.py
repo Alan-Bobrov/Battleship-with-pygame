e475d6f27dc7abdfc855ad5c2c4173b1fc344ef3
AngelesT85 <brawl-ford123456@mail.ru>
2024-08-24 16:58:27 +0300

    Create images.py

---
+++ loads/images.py
@@ -0,0 +1,22 @@
+from pygame.image import load
+from pygame import transform
+
+OneDeckShipImg = load("images/OneDeckShip.png")
+
+ShipStartImg = load("images/ShipStart.png")
+TransformShipStartImg = transform.rotate(ShipStartImg, 90.0)
+
+ShipContinueImg = load("images/ShipContinue.png")
+TransformShipContinueImg = transform.rotate(ShipContinueImg, 90.0)
+
+ShipEndImg = load("images/ShipEnd.png")
+TransformShipEndImg = transform.rotate(ShipEndImg, 90.0)
+
+FieldImg = load("images/Field.png")
+HitImg = load("images/Hit.png")
+SkipImg = load("images/Skip.png")
+RestartImg = load("images/Restart.png")
+YouWinImg = load("images/YouWin.png")
+YouLoseImg = load("images/YouLose.png")
+GameStart = load("images/GameStart.png")
+RandomShipGenImg = load("images/RandomShipGen.png")
