c769f9fbe810c5a375e5c9009e9c44d629756f21
AngelesT85 <brawl-ford123456@mail.ru>
2024-08-25 10:20:26 +0300

    Update settings.py

--- loads/settings.py
+++ loads/settings.py
@@ -11,5 +11,23 @@ with open("settings.json", "r", encoding="utf-8") as settings:
     RandomShipGen = settings["Random Ship Generation"]
     PrintUserCompField = settings["Print User Comp Field"]
     PrintCompCompField = settings["Print Comp Comp Field"]
-    Sounds = settings["Sounds"]
+
+    # sounds settings
+    Sounds = settings["Sounds"] # dict
+    IsSounds = Sounds["Is Sounds"] # bool
+
+    if IsSounds:
+        Language = Sounds["Language"] # ru/en
+        
+        try:
+            if Language.lower() not in ("ru", "en"):
+                Language = "en"
+        except AttributeError:
+            Language = "en"
+
+        Voice = Sounds["Voice"] #dict
+        IsClasic = Voice["Is Classic"] # bool
+        if not IsClasic:
+            IsSwears = Voice["Is Swears"] #bool
+
     IsMusic = settings["Music"]
