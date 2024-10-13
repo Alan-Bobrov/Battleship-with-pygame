d9a3da86d44115a8d831a6c297df720bc8c38876
AngelesT85 <brawl-ford123456@mail.ru>
2024-10-13 20:12:43 +0300

    Update game.py

--- game.py
+++ game.py
@@ -93,8 +93,8 @@ def BattleShip():
             bot_ship_count = 0
 
             if bot_move:
-                pg.display.flip()
                 sleep(DelayBetweenMoves)
+                pg.display.flip()
                 # the bot chooses the place where it goes
                 bot_move, coords, result_of_fire = bot_ob.cell_selection(player_comp_field) # y x
                 y, x = coords
